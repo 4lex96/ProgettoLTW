118af99fdf489db73d1fe0ea3570eb507f3340c4
Alex <munteanu96@gmail.com>
2017-11-30 12:32:14 +0100

    Aggiunto feedback esercizio. Da migliorare leggibilità del codice

--- script/bits_gapfill.js
+++ script/bits_gapfill.js
@@ -5,6 +5,7 @@ function setUpPage(){
 		document.write(questionArray[suindex][0]);
 		document.write("</span><span class='exercise_gapfill'><input size=" + questionArray[suindex][3] + " name=ans" + (suindex+1) + "> </span><span class='exercise_text'>");
               		document.write(questionArray[suindex][2]);
+            		document.write("</span><span class='exercise_marking'><img src='images/transparent.png' name='im" + (suindex +1) + "'></span> </div>");
 
 		}
 		{
@@ -14,15 +15,20 @@ function setUpPage(){
 }
 
 var answerArray = new Array(document.form1.ans1,document.form1.ans2,document.form1.ans3,document.form1.ans4,document.form1.ans5,document.form1.ans6,document.form1.ans7,document.form1.ans8,document.form1.ans9,document.form1.ans10,document.form1.ans11,document.form1.ans12);
+var imageArray = new Array(document.images.im1,document.images.im2,document.images.im3,document.images.im4,document.images.im5,document.images.im6,document.images.im7,document.images.im8,document.images.im9,document.images.im10,document.images.im11,document.images.im12);
 function checkScore() {
 var answerArray = new Array(document.form1.ans1,document.form1.ans2,document.form1.ans3,document.form1.ans4,document.form1.ans5,document.form1.ans6,document.form1.ans7,document.form1.ans8,document.form1.ans9,document.form1.ans10,document.form1.ans11,document.form1.ans12);
+var imageArray = new Array(document.images.im1,document.images.im2,document.images.im3,document.images.im4,document.images.im5,document.images.im6,document.images.im7,document.images.im8,document.images.im9,document.images.im10,document.images.im11,document.images.im12);
 	if(cheat){
 		alert("Please press Start again to try again.")
 	}else{
 		var score = 0;
 		for(csindex = 0;csindex<questionArray.length;csindex++){
 			if (answerArray[csindex].value == questionArray[csindex][1]) {
+				imageArray[csindex].src="images/spunta_copia.png";
 				score++;
+			} else {
+				imageArray[csindex].src="images/cross_copia.png";
 			}
 		}
 		alert("Your score is "+score+" / " + questionArray.length);
@@ -34,21 +40,20 @@ var answerArray = new Array(document.form1.ans1,document.form1.ans2,document.for
 
 function reveal() {
 var answerArray = new Array(document.form1.ans1,document.form1.ans2,document.form1.ans3,document.form1.ans4,document.form1.ans5,document.form1.ans6,document.form1.ans7,document.form1.ans8,document.form1.ans9,document.form1.ans10,document.form1.ans11,document.form1.ans12);
+var imageArray = new Array(document.images.im1,document.images.im2,document.images.im3,document.images.im4,document.images.im5,document.images.im6,document.images.im7,document.images.im8,document.images.im9,document.images.im10,document.images.im11,document.images.im12);
 	for(revindex = 0;revindex<questionArray.length;revindex++){
-		if (answerArray[revindex].value == ""){
-			answerArray[revindex].value = "Risposta esatta:" + " " + questionArray[revindex][1];
-		}
-		else{
-			answerArray[revindex].value = "inserita: " + answerArray[revindex].value + " --> " + " esatta: " + questionArray[revindex][1];
-		}
+		answerArray[revindex].value = questionArray[revindex][1];
+		imageArray[revindex].src="images/transparent.png";
 	}
 	cheat = true;
 }
 function again() {
 var answerArray = new Array(document.form1.ans1,document.form1.ans2,document.form1.ans3,document.form1.ans4,document.form1.ans5,document.form1.ans6,document.form1.ans7,document.form1.ans8,document.form1.ans9,document.form1.ans10,document.form1.ans11,document.form1.ans12);
+var imageArray = new Array(document.images.im1,document.images.im2,document.images.im3,document.images.im4,document.images.im5,document.images.im6,document.images.im7,document.images.im8,document.images.im9,document.images.im10,document.images.im11,document.images.im12);
 	cheat = false;
 	for(agindex = 0;agindex<questionArray.length;agindex++){
 		answerArray[agindex].value = "";
+		imageArray[agindex].src="images/transparent.png";
 	}
 
 }
